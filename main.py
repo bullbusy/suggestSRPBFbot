ca463fbc5e768f2ef2b80bb1132430c231c258e3
Nazar Bulavko <41953384+bullbusy@users.noreply.github.com>
2021-01-29 18:09:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#do not touch anything!
 import asyncio
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
@@ -18,41 +17,42 @@ async def feedback_message_event(message: types.Message):
     await feedback_message(message)
 
 async def feedback_message(message: types.Message):
-
     try:
         if message.chat.id == FEEDBACK_USER_ID:
-            if message.reply_to_message is None:
-                await message.answer(f'To reply to the user, reply to the message sent from him')
+            if message.content_type == "text":
+                await bot.send_message(message.text)
+            elif message.content_type == "photo":
+                capt = message.caption
+                await bot.send_photo(message.photo[-1].file_id,caption=capt)
+            elif message.content_type == "video":
+                capt = message.caption
+                await bot.send_video(message.video.file_id,caption=capt)
+            elif message.content_type == "sticker":
+                await bot.send_sticker(message.sticker.file_id)
+            elif message.content_type == "audio":
+                capt = message.caption
+                await bot.send_audio(message.audio.file_id,caption=capt)
+            elif message.content_type == "voice":
+                capt = message.caption
+                await bot.send_voice(message.voice.file_id,caption=capt)
+            elif message.content_type == "document":
+                capt = message.caption
+                await bot.send_document(message.document.file_id,caption=capt)
+            elif message.content_type == "location":
+                await bot.send_location(message.location)
+            elif message.content_type == "animation":
+                capt = message.caption
+                await bot.send_animation(message.animation.file_id,caption=capt)
+            elif message.content_type == "contact":
+                await bot.send_contact(message.contact.file_id)
+
+        else:
+            if message.forward_from == None:
+                await bot.forward_message(FEEDBACK_USER_ID, message.chat.id, message.message_id)
+                await bot.send_message(message.chat.id, "Очікуй відповіді")
             else:
-                try:
-                    if message.content_type == "text":
-                        await bot.send_message(message.text)
-                    elif message.content_type == "photo":
-                        capt = message.caption
-                        await bot.send_photo(message.photo[-1].file_id,caption=capt)
-                    elif message.content_type == "video":
-                        capt = message.caption
-                        await bot.send_video(message.video.file_id,caption=capt)
-                    elif message.content_type == "sticker":
-                        await bot.send_sticker(message.sticker.file_id)
-                    elif message.content_type == "audio":
-                        capt = message.caption
-                        await bot.send_audio(message.audio.file_id,caption=capt)
-                    elif message.content_type == "voice":
-                        capt = message.caption
-                        await bot.send_voice(message.voice.file_id,caption=capt)
-                    elif message.content_type == "document":
-                        capt = message.caption
-                        await bot.send_document(message.document.file_id,caption=capt)
-                    elif message.content_type == "location":
-                        await bot.send_location(message.location)
-                    elif message.content_type == "animation":
-                        capt = message.caption
-                        await bot.send_animation(message.animation.file_id,caption=capt)
-                    elif message.content_type == "contact":
-                        await bot.send_contact(message.contact.file_id)
-                except aiogram.utils.exceptions.BotBlocked:
-                    await bot.send_message(message.chat.id)
+                await message.answer("Переслані повідомлення не побачать по ту сторону боту")
+    
     except Exception as e:
         await message.answer(message.chat.id,f"{str(e)}")
 
